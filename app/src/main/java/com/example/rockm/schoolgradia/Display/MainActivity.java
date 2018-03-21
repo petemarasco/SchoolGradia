8c626436aa51e3db3fa388a6ff16cd0ce5988219
pmarasc <pmarasc@iastate.edu>
2018-03-20 21:07:22 -0500

    Created the subjectGrade which is a subclass of Score and made some updates to score

--- app/src/main/java/com/example/rockm/schoolgradia/Display/MainActivity.java
+++ app/src/main/java/com/example/rockm/schoolgradia/Display/MainActivity.java
@@ -3,20 +3,24 @@ package com.example.rockm.schoolgradia.Display;
 
 
 
+import android.database.sqlite.SQLiteDatabase;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.database.*;
 
 import com.example.rockm.schoolgradia.Display.MainScreen;
 import com.example.rockm.schoolgradia.R;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    protected SQLiteDatabase subjectDataBase;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        subjectDataBase= openOrCreateDatabase("DBName.db",MODE_PRIVATE,null);
         setContentView(R.layout.activity_main);
         FragmentManager manager= getSupportFragmentManager();
         MainScreen mainFrag= new MainScreen();
---
+++ app/src/main/java/com/example/rockm/schoolgradia/Information/DBHelper.java
@@ -0,0 +1,108 @@
+package com.example.rockm.schoolgradia.Information;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.DatabaseUtils;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static android.R.attr.id;
+import static android.R.attr.version;
+
+/**
+ * Created by rockm on 3/20/2018.
+ */
+
+/**
+ * This Database Handles all of the
+ */
+public class DBHelper extends SQLiteOpenHelper {
+    public static final String DATABASE_NAME = "SubjectDBName.db";
+    public static final String SUBJECTS_TABLE_NAME = "subjects";
+    public static final String SUBJECTS_COLUMN_NAME = "name";
+    public static final String SUBJECTS_COLUMN_GRADE = "grade";
+    public static final String INDIVIDUALS_TABLE_NAME = "individual";
+    public static final String INDIVIDUALS_COLUMN_NAME = "name";
+    public static final String INDIVIDUALS_COLUMN_GRADE = "grade";
+    public static final String INDIVIDUALS_COLUMN_SUBJECT_NAME = "subject_name";
+
+    public DBHelper(Context context) {
+        super(context, DATABASE_NAME,null,1);
+    }
+
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table subjects "+ "(name text primary key, grade integer)");
+        db.execSQL("create table individual "+ "(name text primary key, grade integer, subject_name text)");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        db.execSQL("DROP TABLE IF EXISTS contacts");
+        onCreate(db);
+    }
+
+    public boolean insertSubject(String name, Integer grade){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(SUBJECTS_COLUMN_NAME,name);
+        contentValues.put(SUBJECTS_COLUMN_GRADE,grade);
+        db.insert(SUBJECTS_TABLE_NAME,null,contentValues);
+        return true;
+    }
+
+    public int numberOfSubjects(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, SUBJECTS_TABLE_NAME);
+        return numRows;
+    }
+
+    public boolean updateSubject(String name, Integer grade){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(SUBJECTS_COLUMN_NAME,name);
+        contentValues.put(SUBJECTS_COLUMN_GRADE,grade);
+        db.update(SUBJECTS_TABLE_NAME,contentValues,"name ="+name, null);
+        return true;
+    }
+    public Integer deleteSubject (String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        db.delete("individuals",
+                "subject_name ="+name,
+                null);
+        return db.delete("subjects",
+                "name ="+name,
+                null);
+    }
+    public boolean insertIndividual(String name, Integer grade, String subject_name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(INDIVIDUALS_COLUMN_GRADE,name);
+        contentValues.put( INDIVIDUALS_COLUMN_GRADE,grade);
+        contentValues.put(INDIVIDUALS_COLUMN_SUBJECT_NAME,subject_name);
+        db.insert(INDIVIDUALS_TABLE_NAME,null,contentValues);
+        return true;
+    }
+    public int numberOfindividuals(){
+        SQLiteDatabase db = this.getReadableDatabase();
+        int numRows = (int) DatabaseUtils.queryNumEntries(db, INDIVIDUALS_TABLE_NAME);
+        return numRows;
+    }
+    public boolean updateIndividual(String name, Integer grade, String subject_name){
+        SQLiteDatabase db = this.getWritableDatabase();
+        ContentValues contentValues= new ContentValues();
+        contentValues.put(INDIVIDUALS_COLUMN_GRADE,name);
+        contentValues.put( INDIVIDUALS_COLUMN_GRADE,grade);
+        contentValues.put(INDIVIDUALS_COLUMN_SUBJECT_NAME,subject_name);
+        db.update(INDIVIDUALS_TABLE_NAME,contentValues,"name ="+name, null);
+        return true;
+    }
+    public Integer deleteIndividual (String name) {
+        SQLiteDatabase db = this.getWritableDatabase();
+        return db.delete("individuals",
+                "name ="+name,
+                null);
+    }
+
+}
